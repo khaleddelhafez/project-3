b05ec93a1008525e212352c30d7d08b56896084a
khaled <khaledelhafiz@gmail.com>
2025-02-20 22:10:25 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 var copybtn = document.querySelectorAll(".copybtn")
-
+var copytitle = document.querySelectorAll(".copytitle")
 copybtn.forEach (function(btn){
     btn.onclick = function(){
-    alert("تم نسخ النص.")
+    alert("تم نسخ النص."); 
 }
 }) 
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
